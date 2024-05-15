5ec339d9553626d8e28c358941c8037fd8286474
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2024-05-15 14:19:12 +0200

    Use Board class in josekis page

---
+++ GosujiServer/wwwroot/js/pages/josekis/classes/JosekisBoard.js
@@ -0,0 +1,25 @@
+class JosekisBoard extends Board {
+    constructor() {
+        super();
+
+        this.besogoOptions.panels = "control+names+tree+comment+file";
+    }
+
+    init(boardsize, handicap, sgf) {
+        super.init(boardsize, handicap, sgf);
+    }
+
+    clear(boardsize, handicap, sgf) {
+        super.clear(boardsize, handicap, sgf);
+    
+        // document.querySelector('#game button[title="Variants: [child]/sibling"]').remove();
+        // document.querySelector('#game button[title="Variants: show/[hide]"]').remove();
+        // document.querySelector('#game input[value="9x9"]').remove();
+        // document.querySelector('#game input[value="13x13"]').remove();
+        // document.querySelector('#game input[value="19x19"]').remove();
+        // document.querySelector('#game input[value="?x?"]').remove();
+        // document.querySelector('#game input[value="Comment"]').remove();
+        // document.querySelector('#game input[value="Edit Info"]').remove();
+        // document.querySelector('#game input[value="Info"]').remove();
+    }
+}
---
+++ GosujiServer/wwwroot/js/pages/josekis/josekis.js
@@ -0,0 +1,6 @@
+var josekisPage = {};
+
+josekisPage.init = function () {
+    josekisPage.board = new JosekisBoard();
+    josekisPage.board.init(19, 0);
+};
