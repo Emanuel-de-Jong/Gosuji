4d6d499954eaf031bdba64554c9214202a494e94
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2024-05-22 13:36:38 +0200

    AbuseDetect user subscription and total visits

--- GosujiServer/Shared/CMS/AbuseDetect.razor.cs
+++ GosujiServer/Shared/CMS/AbuseDetect.razor.cs
@@ -1,5 +1,6 @@
 ﻿using GosujiServer.Areas.Identity.Data;
 using GosujiServer.Data;
+using GosujiServer.Pages;
 using GosujiServer.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
@@ -15,11 +16,28 @@ namespace GosujiServer.Shared.CMS
         private DbService dbService { get; set; }
 
         private List<User> users;
+        private Dictionary<string, UserSubscription> subscriptions;
+        private Dictionary<string, long> totalVisits;
 
         protected override async Task OnInitializedAsync()
         {
             ApplicationDbContext dbContext = await dbService.GetContextAsync();
             users = await dbContext.Users.ToListAsync();
+            subscriptions = await dbContext.UserSubscriptions.Include(us => us.SubscriptionType).ToDictionaryAsync(us => us.UserId);
+
+            totalVisits = new();
+            foreach (UserMoveCount moveCount in await dbContext.UserMoveCounts.ToListAsync())
+            {
+                if (!totalVisits.ContainsKey(moveCount.UserId))
+                {
+                    totalVisits[moveCount.UserId] = moveCount.Visits;
+                }
+                else
+                {
+                    totalVisits[moveCount.UserId] += moveCount.Visits;
+                }
+            }
+
             await dbContext.DisposeAsync();
         }
 
