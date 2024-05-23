5a46a1674d4a8b905961597d5b62fb804dd47067
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2024-05-23 11:39:34 +0200

    CMS authorization

---
+++ GosujiServer/Shared/CRedirect.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GosujiServer.Shared
+{
+    public partial class CRedirect : ComponentBase
+    {
+        [Parameter]
+        public string? URI { get; set; }
+
+        [Inject]
+        private NavigationManager? navigationManager { get; set; }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            navigationManager.NavigateTo("/" + URI);
+        }
+    }
+}
