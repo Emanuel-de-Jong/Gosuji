1987300a55b08e9c018b43bcf1bb309875fb9130
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2024-05-31 15:54:57 +0200

    Add rollup for js bundling and minification

---
+++ Gosuji/Gosuji/rollup.config.js
@@ -0,0 +1,19 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import multiEntry from '@rollup/plugin-multi-entry';
+import terser from '@rollup/plugin-terser';
+
+export default {
+    input: 'wwwroot/js/pages/josekis/**/*.js',
+    output: {
+        file: 'wwwroot/js/pages/josekis/bundle.min.js',
+        format: 'es',
+        sourcemap: true,
+        plugins: [terser()]
+    },
+    plugins: [
+        multiEntry(),
+        resolve(), // Resolve modules from node_modules
+        commonjs() // Convert CommonJS modules to ES6
+    ]
+};
