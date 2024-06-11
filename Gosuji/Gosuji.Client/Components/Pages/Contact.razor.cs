591d7579397ffea386a99e1229a7d663bdd38922
Emanuel de Jong <emanueldejong@hotmail.com>
2024-06-11 14:26:56 +0200

    Contact save feedback. Validation still to do.

--- Gosuji/Gosuji.Client/Components/Pages/Contact.razor.cs
+++ Gosuji/Gosuji.Client/Components/Pages/Contact.razor.cs
@@ -1,9 +1,49 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Gosuji.Client.Data;
+using Gosuji.Client.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
 
 namespace Gosuji.Client.Components.Pages
 {
     public partial class Contact : ComponentBase
     {
+        [Inject]
+        private AuthenticationStateProvider authenticationStateProvider { get; set; }
+        [Inject]
+        private IDataService dataService { get; set; }
 
+        private string? userId;
+
+        private string? subject;
+        private string? message;
+        private EFeedbackType feedbackType;
+
+        protected override async Task OnInitializedAsync()
+        {
+            ClaimsPrincipal claimsPrincipal = (await authenticationStateProvider.GetAuthenticationStateAsync()).User;
+            if (claimsPrincipal.Identity != null && claimsPrincipal.Identity.IsAuthenticated)
+            {
+                userId = claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            }
+        }
+
+        private async Task SendFeedback()
+        {
+            if (userId == null)
+            {
+                return;
+            }
+
+            Feedback feedback = new()
+            {
+                UserId = userId,
+                Subject = subject,
+                Message = message,
+                FeedbackType = feedbackType
+            };
+
+            await dataService.PostFeedback(feedback);
+        }
     }
 }
