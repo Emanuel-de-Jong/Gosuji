0954b0d42d692ebf5254b46bc5d26041658fc7f2
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2024-06-12 16:34:19 +0200

    Home IStringLocalizer example

--- Gosuji/Gosuji.Client/Resources/Translations/General.Designer.cs
+++ Gosuji/Gosuji.Client/Resources/Translations/General.Designer.cs
@@ -22,14 +22,14 @@ namespace Gosuji.Client.Resources.Translations {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class ValidateMessages {
+    public class General {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal ValidateMessages() {
+        internal General() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Gosuji.Client.Resources.Translations {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Gosuji.Client.Resources.Translations.ValidateMessages", typeof(ValidateMessages).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Gosuji.Client.Resources.Translations.General", typeof(General).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -63,9 +63,9 @@ namespace Gosuji.Client.Resources.Translations {
         /// <summary>
         ///   Looks up a localized string similar to .
         /// </summary>
-        public static string RequiredError {
+        public static string Home_Description {
             get {
-                return ResourceManager.GetString("RequiredError", resourceCulture);
+                return ResourceManager.GetString("Home_Description", resourceCulture);
             }
         }
     }
