d87889c41dc7b659a5c853c3abbb8dc0ab509786
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2024-06-18 23:33:27 +0200

    StringLengths migration

---
+++ Gosuji/Gosuji/Migrations/20240618213305_StringLengths.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gosuji.Migrations
+{
+    /// <inheritdoc />
+    public partial class StringLengths : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Flag",
+                table: "Languages");
+
+            migrationBuilder.DropColumn(
+                name: "Thumbnail",
+                table: "Games");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Flag",
+                table: "Languages",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Thumbnail",
+                table: "Games",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
