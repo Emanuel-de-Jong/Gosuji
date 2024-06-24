81548321c4c169ac8b5ea65970929c017e70b526
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2024-06-24 16:32:56 +0200

    RateLimitSetup helper class

---
+++ Gosuji/Gosuji/Helpers/RateLimitSetup.cs
@@ -0,0 +1,57 @@
+﻿using Gosuji.Client;
+using Gosuji.Services;
+using System.Globalization;
+using System.Threading.RateLimiting;
+
+namespace Gosuji.Helpers
+{
+    public class RateLimitSetup
+    {
+        public static void AddRateLimiters(WebApplicationBuilder builder)
+        {
+            builder.Services.AddSingleton<RateLimitLogger>();
+
+            builder.Services.AddRateLimiter(options =>
+            {
+                options.AddPolicy(G.RazorRateLimitPolicyName, context =>
+                {
+                    string ipAddress = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+                    return RateLimitPartition.GetFixedWindowLimiter(ipAddress, partition => new()
+                    {
+                        PermitLimit = 100,
+                        Window = TimeSpan.FromSeconds(10),
+                        QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                        QueueLimit = 0
+                    });
+                });
+
+                options.AddPolicy(G.ControllerRateLimitPolicyName, context =>
+                {
+                    string ipAddress = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+                    return RateLimitPartition.GetFixedWindowLimiter(ipAddress, partition => new()
+                    {
+                        PermitLimit = 100,
+                        Window = TimeSpan.FromSeconds(10),
+                        QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                        QueueLimit = 0
+                    });
+                });
+
+                options.OnRejected = async (context, cancellationToken) =>
+                {
+                    if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+                    {
+                        context.HttpContext.Response.Headers.RetryAfter =
+                            ((int)retryAfter.TotalSeconds).ToString(NumberFormatInfo.InvariantInfo);
+                    }
+
+                    context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+                    await context.HttpContext.Response.WriteAsync("Too Many Requests");
+
+                    RateLimitLogger logger = context.HttpContext.RequestServices.GetRequiredService<RateLimitLogger>();
+                    logger.LogViolation(context.HttpContext);
+                };
+            });
+        }
+    }
+}
--- Gosuji/Gosuji/Program.cs
+++ Gosuji/Gosuji/Program.cs
@@ -8,8 +8,6 @@ using Gosuji.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using System.Globalization;
-using System.Threading.RateLimiting;
 
 namespace Gosuji
 {
@@ -47,50 +45,7 @@ namespace Gosuji
                 .AddSignInManager()
                 .AddDefaultTokenProviders();
 
-            builder.Services.AddSingleton<RateLimitLogger>();
-
-            builder.Services.AddRateLimiter(options =>
-            {
-                options.AddPolicy(G.RazorRateLimitPolicyName, context =>
-                {
-                    string ipAddress = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
-                    return RateLimitPartition.GetFixedWindowLimiter(ipAddress, partition => new()
-                    {
-                        PermitLimit = 100,
-                        Window = TimeSpan.FromSeconds(10),
-                        QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
-                        QueueLimit = 0
-                    });
-                });
-
-                options.AddPolicy(G.ControllerRateLimitPolicyName, context =>
-                {
-                    string ipAddress = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
-                    return RateLimitPartition.GetFixedWindowLimiter(ipAddress, partition => new()
-                    {
-                        PermitLimit = 100,
-                        Window = TimeSpan.FromSeconds(10),
-                        QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
-                        QueueLimit = 0
-                    });
-                });
-
-                options.OnRejected = async (context, cancellationToken) =>
-                {
-                    if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
-                    {
-                        context.HttpContext.Response.Headers.RetryAfter =
-                            ((int)retryAfter.TotalSeconds).ToString(NumberFormatInfo.InvariantInfo);
-                    }
-
-                    context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
-                    await context.HttpContext.Response.WriteAsync("Too Many Requests");
-
-                    RateLimitLogger logger = context.HttpContext.RequestServices.GetRequiredService<RateLimitLogger>();
-                    logger.LogViolation(context.HttpContext);
-                };
-            });
-
+            RateLimitSetup.AddRateLimiters(builder);
 
             builder.Services.AddControllers();
 
