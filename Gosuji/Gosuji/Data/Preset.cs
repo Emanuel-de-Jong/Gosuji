f3abc6b2a10f058f1af3fd575272efbaae9459e6
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2024-06-25 10:58:00 +0200

    Preset UserId nullable (general preset)

--- Gosuji/Gosuji/Data/Preset.cs
+++ Gosuji/Gosuji/Data/Preset.cs
@@ -1,22 +1,27 @@
 ﻿using Gosuji.Client.Data;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Gosuji.Data
 {
     public class Preset : DbModel
     {
         [Key] public long Id { get; set; }
-        [Required]
+
         [StringLength(36)]
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
         public User? User { get; set; }
         [Required]
         public long TrainerSettingConfigId { get; set; }
         public TrainerSettingConfig? TrainerSettingConfig { get; set; }
+
         [Required]
         [MaxLength(250)]
         [MinLength(1)]
         public string Name { get; set; }
         public bool IsDeleted { get; set; }
+
+        [NotMapped]
+        public bool IsGeneral => string.IsNullOrEmpty(UserId);
     }
 }
