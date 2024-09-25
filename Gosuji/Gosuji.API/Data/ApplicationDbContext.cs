759fc68c85ae3b8a8de75e7bc7cff638240d336f
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2024-09-25 23:29:35 +0200

    .

--- Gosuji/Gosuji.API/Data/ApplicationDbContext.cs
+++ Gosuji/Gosuji.API/Data/ApplicationDbContext.cs
@@ -66,18 +66,9 @@ namespace Gosuji.API.Data
         {
             base.OnModelCreating(builder);
 
-            builder.Entity<TrainerSettingConfig>()
-                .HasIndex(e => e.Hash)
-                .IsUnique();
-
             builder.Entity<PendingUserChange>()
                 .HasOne(puc => puc.User)
                 .WithOne()
-                .HasForeignKey<PendingUserChange>(puc => puc.Id)
-                .OnDelete(DeleteBehavior.Cascade);
-            builder.Entity<User>()
-                .HasOne<PendingUserChange>()
-                .WithOne(puc => puc.User)
                 .HasForeignKey<PendingUserChange>(puc => puc.Id)
                 .OnDelete(DeleteBehavior.SetNull);
 
@@ -85,11 +76,12 @@ namespace Gosuji.API.Data
                 .HasOne<User>()
                 .WithMany()
                 .HasForeignKey(rt => rt.UserId)
-                .OnDelete(DeleteBehavior.Cascade);
+                .OnDelete(DeleteBehavior.SetNull);
+
             builder.Entity<User>()
-                .HasMany<RefreshToken>()
-                .WithOne()
-                .HasForeignKey(rt => rt.UserId)
+                .HasOne(u => u.CurrentSubscription)
+                .WithMany()
+                .HasForeignKey(u => u.CurrentSubscriptionId)
                 .OnDelete(DeleteBehavior.SetNull);
 
             builder.Entity<UserActivity>()
@@ -97,21 +89,17 @@ namespace Gosuji.API.Data
                 .WithMany()
                 .HasForeignKey(ua => ua.UserId)
                 .OnDelete(DeleteBehavior.SetNull);
-            builder.Entity<User>()
-                .HasMany<UserActivity>()
-                .WithOne(ua => ua.User)
-                .HasForeignKey(ua => ua.UserId)
-                .OnDelete(DeleteBehavior.SetNull);
 
             builder.Entity<UserMoveCount>()
                 .HasOne(umc => umc.User)
                 .WithMany()
                 .HasForeignKey(umc => umc.UserId)
-                .OnDelete(DeleteBehavior.Cascade);
-            builder.Entity<User>()
-                .HasMany<UserMoveCount>()
-                .WithOne(umc => umc.User)
-                .HasForeignKey(umc => umc.UserId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.Entity<EncodedGameData>()
+                .HasOne<Game>()
+                .WithOne(g => g.EncodedGameData)
+                .HasForeignKey<EncodedGameData>(egd => egd.Id)
                 .OnDelete(DeleteBehavior.SetNull);
 
             builder.Entity<Feedback>()
@@ -119,108 +107,46 @@ namespace Gosuji.API.Data
                 .WithMany()
                 .HasForeignKey(fb => fb.UserId)
                 .OnDelete(DeleteBehavior.SetNull);
-            builder.Entity<User>()
-                .HasMany<Feedback>()
-                .WithOne()
-                .HasForeignKey(fb => fb.UserId)
-                .OnDelete(DeleteBehavior.SetNull);
-
-            builder.Entity<Game>()
-                .HasOne(g => g.EncodedGameData)
-                .WithOne()
-                .HasForeignKey<EncodedGameData>(egd => egd.Id)
-                .OnDelete(DeleteBehavior.NoAction);
-            builder.Entity<EncodedGameData>()
-                .HasOne<Game>()
-                .WithOne(g => g.EncodedGameData)
-                .HasForeignKey<EncodedGameData>(egd => egd.Id)
-                .OnDelete(DeleteBehavior.Cascade);
 
             builder.Entity<Game>()
                 .HasOne(g => g.TrainerSettingConfig)
                 .WithMany()
                 .HasForeignKey(g => g.TrainerSettingConfigId)
                 .OnDelete(DeleteBehavior.SetNull);
-            builder.Entity<TrainerSettingConfig>()
-                .HasMany<Game>()
-                .WithOne(g => g.TrainerSettingConfig)
-                .HasForeignKey(g => g.TrainerSettingConfigId)
-                .OnDelete(DeleteBehavior.SetNull);
-
             builder.Entity<Game>()
                 .HasOne(g => g.KataGoVersion)
                 .WithMany()
                 .HasForeignKey(g => g.KataGoVersionId)
                 .OnDelete(DeleteBehavior.SetNull);
-            builder.Entity<KataGoVersion>()
-                .HasMany<Game>()
-                .WithOne(g => g.KataGoVersion)
-                .HasForeignKey(g => g.KataGoVersionId)
-                .OnDelete(DeleteBehavior.SetNull);
-
             builder.Entity<Game>()
                 .HasOne(g => g.GameStat)
                 .WithMany()
                 .HasForeignKey(g => g.GameStatId)
                 .OnDelete(DeleteBehavior.SetNull);
-            builder.Entity<GameStat>()
-                .HasMany<Game>()
-                .WithOne(g => g.GameStat)
-                .HasForeignKey(g => g.GameStatId)
-                .OnDelete(DeleteBehavior.Cascade);
-
             builder.Entity<Game>()
                 .HasOne(g => g.OpeningStat)
                 .WithMany()
                 .HasForeignKey(g => g.OpeningStatId)
                 .OnDelete(DeleteBehavior.SetNull);
-            builder.Entity<GameStat>()
-                .HasMany<Game>()
-                .WithOne(g => g.OpeningStat)
-                .HasForeignKey(g => g.OpeningStatId)
-                .OnDelete(DeleteBehavior.Cascade);
-
             builder.Entity<Game>()
                 .HasOne(g => g.MidgameStat)
                 .WithMany()
                 .HasForeignKey(g => g.MidgameStatId)
                 .OnDelete(DeleteBehavior.SetNull);
-            builder.Entity<GameStat>()
-                .HasMany<Game>()
-                .WithOne(g => g.MidgameStat)
-                .HasForeignKey(g => g.MidgameStatId)
-                .OnDelete(DeleteBehavior.Cascade);
-
             builder.Entity<Game>()
                 .HasOne(g => g.EndgameStat)
                 .WithMany()
                 .HasForeignKey(g => g.EndgameStatId)
                 .OnDelete(DeleteBehavior.SetNull);
-            builder.Entity<GameStat>()
-                .HasMany<Game>()
-                .WithOne(g => g.EndgameStat)
-                .HasForeignKey(g => g.EndgameStatId)
-                .OnDelete(DeleteBehavior.Cascade);
 
             builder.Entity<Preset>()
                 .HasOne<User>()
                 .WithMany()
                 .HasForeignKey(p => p.UserId)
-                .OnDelete(DeleteBehavior.Cascade);
-            builder.Entity<User>()
-                .HasMany<Preset>()
-                .WithOne()
-                .HasForeignKey(p => p.UserId)
                 .OnDelete(DeleteBehavior.SetNull);
-
             builder.Entity<Preset>()
                 .HasOne(p => p.TrainerSettingConfig)
                 .WithMany()
-                .HasForeignKey(p => p.TrainerSettingConfigId)
-                .OnDelete(DeleteBehavior.Cascade);
-            builder.Entity<TrainerSettingConfig>()
-                .HasMany<Preset>()
-                .WithOne(p => p.TrainerSettingConfig)
                 .HasForeignKey(p => p.TrainerSettingConfigId)
                 .OnDelete(DeleteBehavior.SetNull);
 
@@ -229,59 +155,31 @@ namespace Gosuji.API.Data
                 .WithMany()
                 .HasForeignKey(sc => sc.LanguageId)
                 .OnDelete(DeleteBehavior.SetNull);
-            builder.Entity<Language>()
-                .HasMany<SettingConfig>()
-                .WithOne(sc => sc.Language)
-                .HasForeignKey(sc => sc.LanguageId)
-                .OnDelete(DeleteBehavior.SetNull);
 
             builder.Entity<Subscription>()
                 .HasOne<User>()
                 .WithMany()
                 .HasForeignKey(s => s.UserId)
-                .OnDelete(DeleteBehavior.Cascade);
-            builder.Entity<User>()
-                .HasMany<Subscription>()
-                .WithOne()
-                .HasForeignKey(s => s.UserId)
                 .OnDelete(DeleteBehavior.SetNull);
-            builder.Entity<User>()
-                .HasOne(u => u.CurrentSubscription)
-                .WithMany()
-                .HasForeignKey(u => u.CurrentSubscriptionId)
-                .OnDelete(DeleteBehavior.SetNull);
-
             builder.Entity<Subscription>()
                 .HasOne(s => s.Discount)
                 .WithMany()
                 .HasForeignKey(s => s.DiscountId)
                 .OnDelete(DeleteBehavior.SetNull);
-            builder.Entity<Discount>()
-                .HasMany<Subscription>()
-                .WithOne(s => s.Discount)
-                .HasForeignKey(s => s.DiscountId)
-                .OnDelete(DeleteBehavior.SetNull);
 
-            builder.Entity<UserState>()
-                .HasOne(us => us.LastPreset)
-                .WithMany()
-                .HasForeignKey(us => us.LastPresetId)
-                .OnDelete(DeleteBehavior.SetNull);
-            builder.Entity<Preset>()
-                .HasMany<UserState>()
-                .WithOne(us => us.LastPreset)
-                .HasForeignKey(us => us.LastPresetId)
-                .OnDelete(DeleteBehavior.SetNull);
+            builder.Entity<TrainerSettingConfig>()
+                .HasIndex(e => e.Hash)
+                .IsUnique();
 
             builder.Entity<UserState>()
                 .HasOne<User>()
                 .WithOne()
                 .HasForeignKey<UserState>(us => us.Id)
-                .OnDelete(DeleteBehavior.Cascade);
-            builder.Entity<User>()
-                .HasOne<UserState>()
-                .WithOne()
-                .HasForeignKey<UserState>(us => us.Id)
+                .OnDelete(DeleteBehavior.SetNull);
+            builder.Entity<UserState>()
+                .HasOne(us => us.LastPreset)
+                .WithMany()
+                .HasForeignKey(us => us.LastPresetId)
                 .OnDelete(DeleteBehavior.SetNull);
         }
     }
---
+++ Gosuji/Gosuji.API/Migrations/20240925212844_OnDeleteSetNull.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gosuji.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class OnDeleteSetNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EncodedGameDatas_Games_Id",
+                table: "EncodedGameDatas");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_GameStats_EndgameStatId",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_GameStats_GameStatId",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_GameStats_MidgameStatId",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_GameStats_OpeningStatId",
+                table: "Games");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EncodedGameDatas_Games_Id",
+                table: "EncodedGameDatas",
+                column: "Id",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_GameStats_EndgameStatId",
+                table: "Games",
+                column: "EndgameStatId",
+                principalTable: "GameStats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_GameStats_GameStatId",
+                table: "Games",
+                column: "GameStatId",
+                principalTable: "GameStats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_GameStats_MidgameStatId",
+                table: "Games",
+                column: "MidgameStatId",
+                principalTable: "GameStats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_GameStats_OpeningStatId",
+                table: "Games",
+                column: "OpeningStatId",
+                principalTable: "GameStats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EncodedGameDatas_Games_Id",
+                table: "EncodedGameDatas");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_GameStats_EndgameStatId",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_GameStats_GameStatId",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_GameStats_MidgameStatId",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_GameStats_OpeningStatId",
+                table: "Games");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EncodedGameDatas_Games_Id",
+                table: "EncodedGameDatas",
+                column: "Id",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_GameStats_EndgameStatId",
+                table: "Games",
+                column: "EndgameStatId",
+                principalTable: "GameStats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_GameStats_GameStatId",
+                table: "Games",
+                column: "GameStatId",
+                principalTable: "GameStats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_GameStats_MidgameStatId",
+                table: "Games",
+                column: "MidgameStatId",
+                principalTable: "GameStats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_GameStats_OpeningStatId",
+                table: "Games",
+                column: "OpeningStatId",
+                principalTable: "GameStats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
