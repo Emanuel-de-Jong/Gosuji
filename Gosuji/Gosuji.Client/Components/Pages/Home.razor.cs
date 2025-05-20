e841041734fdd7837b50cb2ee335283637a7412b
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2025-05-20 19:44:24 +0200

    Home class

---
+++ Gosuji/Gosuji.Client/Components/Pages/Home.razor.cs
@@ -0,0 +1,37 @@
+﻿using Gosuji.Client.Data;
+using Gosuji.Client.Resources.Translations;
+using Gosuji.Client.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.Localization;
+
+namespace Gosuji.Client.Components.Pages
+{
+    public partial class Home : CustomPage
+    {
+        [Inject]
+        private AuthenticationStateProvider authenticationStateProvider { get; set; }
+        [Inject]
+        private DataAPI dataAPI { get; set; }
+        [Inject]
+        private NavigationManager navigationManager { get; set; }
+        [Inject]
+        private IStringLocalizer<General> tl { get; set; }
+
+        private Changelog[]? changelogs;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            // navigationManager.NavigateTo("learn/trainer");
+            // return;
+
+            changelogs = await dataAPI.GetChangelogs();
+            if (changelogs != null)
+            {
+                changelogs = changelogs.OrderByDescending(c => c.Date).ToArray();
+            }
+        }
+    }
+}
